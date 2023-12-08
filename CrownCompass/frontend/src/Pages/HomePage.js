81e250a763baf89322c0f28be64c7939fcaeb48f
Olivia Adoghe <oliviaadoghe@gmail.com>
2023-12-08 14:33:59 -0500

    oli

--- CrownCompass/frontend/src/Pages/HomePage.js
+++ CrownCompass/frontend/src/Pages/HomePage.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { FaGoogle, FaFacebook, FaInstagram } from 'react-icons/fa';
 
-//import { ReactComponent as EmailIcon } from './icons/email.svg'; // Use actual path to your SVG icon
-//import { ReactComponent as PasswordIcon } from './icons/password.svg'; // Use actual path to your SVG icon
+// Import icons if needed
+// import { ReactComponent as EmailIcon } from './icons/email.svg';
+// import { ReactComponent as PasswordIcon } from './icons/password.svg';
+
+const SocialMediaIcons = () => {
+    const googleLink = 'https://www.google.com/';
+    const facebookLink = 'https://www.facebook.com/';
+    const instagramLink = 'https://www.instagram.com/';
+
+    return (
+        <div>
+            <a href={googleLink} target="_blank" rel="noopener noreferrer">
+                <FaGoogle size={32} style={{ margin: '5px', color: '#4285F4' }} />
+            </a>
+            <a href={facebookLink} target="_blank" rel="noopener noreferrer">
+                <FaFacebook size={32} style={{ margin: '5px', color: '#4267B2' }} />
+            </a>
+            <a href={instagramLink} target="_blank" rel="noopener noreferrer">
+                <FaInstagram size={32} style={{ margin: '5px', color: '#E4405F' }} />
+            </a>
+        </div>
+    );
+};
 
 export default function HomePage() {
     // ... rest remains the same
@@ -32,16 +54,16 @@ export default function HomePage() {
                         <input type="checkbox" id="terms" />
                         <label htmlFor="terms">I agree to the Terms & Conditions</label>
                     </div>
-                    <button type="submit" className="sign-up-button">Sign Up</button>
+                    <button type="submit" className="sign-up-button">
+                        Sign Up
+                    </button>
                 </form>
-                <div className="social-buttons">
-                    {/* Replace with actual icons */}
-                    <div className="social-icon">G</div>
-                    <div className="social-icon">F</div>
-                    <div className="social-icon">A</div>
-                </div>
+                {/* Include SocialMediaIcons component */}
+                <SocialMediaIcons />
                 <div className="home-footer">
-                    <p>Already have an account? <a href="/login">Sign in</a></p>
+                    <p>
+                        Already have an account? <a href="/login">Sign in</a>
+                    </p>
                 </div>
             </div>
         </div>
