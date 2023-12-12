203d16cf1679b5c1698a199f56b2c025fa85d188
xavthom0104 <xavier.thomas0104@yahoo.com>
2023-12-12 13:04:43 -0500

    New UserService *thumbs up emoji*

--- CrownCompass/src/main/java/com/example/crowncompass/service/UserService.java
+++ CrownCompass/src/main/java/com/example/crowncompass/service/UserService.java
@@ -33,88 +33,88 @@ public class UserService{
 
 
     }
-        public ArrayList<User> getAllUser ()throws ExecutionException, InterruptedException {
-            CollectionReference userCollection = firestore.collection("User");
-            ApiFuture<QuerySnapshot> future = userCollection.get();
+    public ArrayList<User> getAllUser ()throws ExecutionException, InterruptedException {
+        CollectionReference userCollection = firestore.collection("User");
+        ApiFuture<QuerySnapshot> future = userCollection.get();
 
-            ArrayList<User> bookingsList = new ArrayList<>();
+        ArrayList<User> bookingsList = new ArrayList<>();
 
-            for (DocumentSnapshot document : future.get().getDocuments()) {
-                User user = documentSnapshotToHotel(document);
-                if (user != null)
-                    bookingsList.add(user);
-            }
-            return bookingsList;
-
+        for (DocumentSnapshot document : future.get().getDocuments()) {
+            User user = documentSnapshotToHotel(document);
+            if (user != null)
+                bookingsList.add(user);
         }
+        return bookingsList;
 
-        public User getUserById (String userID) throws ExecutionException, InterruptedException {
-            CollectionReference bookingsCollection = firestore.collection("User");
-            ApiFuture<DocumentSnapshot> future = bookingsCollection.document(userID).get();
-            DocumentSnapshot document = future.get();
-            return documentSnapshotToHotel(document);
-        }
-        public String createUser (User user) throws ExecutionException, InterruptedException {
-            String userId = null;
-            ApiFuture<DocumentReference> future = firestore.collection("User").add(user);
-            DocumentReference postRef = future.get();
-            userId = postRef.getId();
-            return userId;
+    }
 
-        }
-        public void updateUser (String id, Map < String, Object > updatedValues) throws ParseException {
+    public User getUserById (String userID) throws ExecutionException, InterruptedException {
+        CollectionReference bookingsCollection = firestore.collection("User");
+        ApiFuture<DocumentSnapshot> future = bookingsCollection.document(userID).get();
+        DocumentSnapshot document = future.get();
+        return documentSnapshotToHotel(document);
+    }
+    public String createUser (User user) throws ExecutionException, InterruptedException {
+        String userId = null;
+        ApiFuture<DocumentReference> future = firestore.collection("User").add(user);
+        DocumentReference postRef = future.get();
+        userId = postRef.getId();
+        return userId;
+
+    }
+    public void updateUser (String id, Map < String, Object > updatedValues) throws ParseException {
         String[] allowed = {"updatedAt", "userLocation", "createdAt", "username"};
-            List<String> list = Arrays.asList(allowed);
-            Map<String, Object> formattedValues = new HashMap<>();
-            Log logger = LogFactory.getLog(this.getClass());
+        List<String> list = Arrays.asList(allowed);
+        Map<String, Object> formattedValues = new HashMap<>();
+        Log logger = LogFactory.getLog(this.getClass());
 
-            for (Map.Entry<String, Object> entry : updatedValues.entrySet()) {
-                String key = entry.getKey();
+        for (Map.Entry<String, Object> entry : updatedValues.entrySet()) {
+            String key = entry.getKey();
 
-                if (list.contains(key)) {
-                    switch(key) {
-                        case "createdAt":
-                        case "updatedAt":
-                            formattedValues.put(key, Timestamp.fromProto(Timestamps.parse((String) entry.getValue())));
-                            break;
+            if (list.contains(key)) {
+                switch(key) {
+                    case "createdAt":
+                    case "updatedAt":
+                        formattedValues.put(key, Timestamp.fromProto(Timestamps.parse((String) entry.getValue())));
+                        break;
 
-                        case "userLocation":
-                            double latitude, longitude;
-                            HashMap<String,Double> latlon = (HashMap<String,Double>) entry.getValue();
-                            latitude = latlon.get("latitude");
-                            longitude = latlon.get("longitude");
-                            GeoPoint geoPoint = new GeoPoint(latitude,longitude);
-                            formattedValues.put(key,geoPoint);
-                            break;
-                        default:
-                            formattedValues.put(key, (String) entry.getValue());
-                            break;
-                    }
+                    case "userLocation":
+                        double latitude, longitude;
+                        HashMap<String,Double> latlon = (HashMap<String,Double>) entry.getValue();
+                        latitude = latlon.get("latitude");
+                        longitude = latlon.get("longitude");
+                        GeoPoint geoPoint = new GeoPoint(latitude,longitude);
+                        formattedValues.put(key,geoPoint);
+                        break;
+                    default:
+                        formattedValues.put(key, (String) entry.getValue());
+                        break;
                 }
             }
-
-            DocumentReference userDoc = firestore.collection("User").document(id);
-            logger.info(formattedValues);
-            if (userDoc != null) {
-                try {
-                    userDoc.update(formattedValues);
-                    System.out.println("Update successful!");
-                } catch (FirestoreException e) {
-                    System.err.println("Error updating document: " + e.getMessage());
-                }
-            } else {
-                System.err.println("Document with ID " + id + " not found!");
-            }
-
         }
-        public void deleteUsers (String userID) throws ExecutionException, InterruptedException {
-            CollectionReference userCollection = firestore.collection("User");
-            DocumentReference future = userCollection.document(userID);
-            future.delete();
 
-
+        DocumentReference userDoc = firestore.collection("User").document(id);
+        logger.info(formattedValues);
+        if (userDoc != null) {
+            try {
+                userDoc.update(formattedValues);
+                System.out.println("Update successful!");
+            } catch (FirestoreException e) {
+                System.err.println("Error updating document: " + e.getMessage());
+            }
+        } else {
+            System.err.println("Document with ID " + id + " not found!");
         }
 
     }
+    public void deleteUsers (String userID) throws ExecutionException, InterruptedException {
+        CollectionReference userCollection = firestore.collection("User");
+        DocumentReference future = userCollection.document(userID);
+        future.delete();
+
+
+    }
+
+}
 
 
