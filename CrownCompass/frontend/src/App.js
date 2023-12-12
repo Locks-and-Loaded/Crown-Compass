723c3ece9856f2139a07f31ab8eddcc4cd3e22dc
xavthom0104 <xavier.thomas0104@yahoo.com>
2023-12-12 12:20:49 -0500

    all new pages load. added places library to Maps API. commented out "ServiceCard" import on HairCareServices.js for the time being

--- CrownCompass/frontend/src/App.js
+++ CrownCompass/frontend/src/App.js
@@ -3,14 +3,20 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 import HomePage from "./Pages/HomePage";
 import MapPage from "./Pages/MapPage";
+import HairCareServices from "./Pages/HairCareServices";
+import ProfilePage from "./Pages/ProfilePage";
 
 function App() {
   return (
       <Router>
         <Routes>
-          <Route path="/login" element={<LoginPage />} />
-            <Route path="/" element={<HomePage />} />
+            <Route path ="/login" element={<LoginPage />} />
+            <Route path ="/home" element={<HomePage />} />
             <Route path ="/map" element={<MapPage/>} />
+            <Route path ="/services" element={<HairCareServices/>} />
+            <Route path ="/profile" element = {<ProfilePage/>}/>
+
+
         </Routes>
       </Router>
   );
--- CrownCompass/frontend/src/Pages/MapPage.js
+++ CrownCompass/frontend/src/Pages/MapPage.js
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from 'react';
-import MapComponent from "../MapComponent";
+import MapComponent from "../components/MapComponent";
 
 const MapPage = () => {
     const [map, setMap] = useState(null);
     const [searchBox, setSearchBox] = useState(null);
     const [marker, setMarker] = useState(null);
 
-        const initialCoordinates = {lat: -3.745, lng: -38.523};
+
+        /* needs a function that can pull the user's location from Firebase.
+         for now, it defaults to Tallahassee. */
+        const initialCoordinates = {lat: 30.4382, lng: -84.2533};
         const zoomLevel = 10;
         const markers = [
-            {coordinates: {lat: -3.745, lng: -38.523}, title: 'Marker 1'},
+            {coordinates: {lat: 30.4382, lng: -84.2533}, title: 'Marker 1'},
             // Add more markers as needed
         ];
 
         return (
             <div>
-                <input id="search-input" type="text" placeholder="Search for hairstylists"/>
+                <input id="search-input" type="text" placeholder="Search for stylists in your area"/>
                 <MapComponent initialCoordinates={initialCoordinates}
                               zoomLevel={zoomLevel}
                               markers={markers}/>
